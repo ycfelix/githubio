113542c2f2f5faf0afc6d64b65d0faba8f2ef18e
YCFELIX\yauch <ycfelix7@gmail.com>
2020-02-18 12:41:21 +0800

    not working...revert

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          One small step for a man
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learning now
-        </a>
       </header>
     </div>
   );
